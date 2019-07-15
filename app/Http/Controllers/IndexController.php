068e1f8a1d81a6080d9a050c412e9e69c1cd8c6a
vadim <vadim50>
2019-07-15 15:25:34 +0300

    Lesson 46

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,6 +7,7 @@ use App\Page;
 use App\Service;
 use App\Portfolio;
 use App\People;
+use DB;
 
 class IndexController extends Controller
 {
@@ -18,6 +19,8 @@ class IndexController extends Controller
     	$services = Service::where('id','<',20)->get();
     	$peoples = People::take(3)->get();
 
+    	$tags = DB::table('portfolios')->distinct()->pluck('filter');
+    	
     	$menu = [];
 
     	foreach($pages as $page){
@@ -42,7 +45,8 @@ class IndexController extends Controller
     		'pages'=>$pages,
     		'services'=>$services,
     		'portfolios'=>$portfolios,
-    		'peoples'=>$peoples
+    		'peoples'=>$peoples,
+    		'tags'=>$tags
 
     	]);
     }
